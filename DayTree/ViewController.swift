894c8f6bdfae4986703b0eb569c6d19b67c4c90b
Yoshi <yoshinori.shibahara@gmail.com>
2017-03-19 16:54:25 +0900

    Spring Camp 2017 Day 1

--- DayTree/ViewController.swift
+++ DayTree/ViewController.swift
@@ -20,8 +20,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // section毎の画像配列
     var imgArray: [String] = ["img0.jpg","img1.jpg","img2.jpg","img3.jpg", "img4.jpg","img5.jpg","img6.jpg","img7.jpg"]
     
-    var label2Array: [String] = ["2017/02/10","2017/02/9","2017/02/8","2017/02/7",
+    var entryArray: [String] = ["2017/02/10","2017/02/9","2017/02/8","2017/02/7",
                                 "2017/02/6","2017/02/5","2017/02/4","2017/02/3"]
+    
+    var dateArray: [String] = ["↑First Day↑"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,33 +34,61 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
-    // aaa
-    
-
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        
-        // TableViewを編集可能にする
-        dateTableView.setEditing(editing, animated: true)
-        
-        // 編集中のときのみaddButtonをナビゲーションバーの左に表示する
-        if editing {
-            print("編集中")
-            let addButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(ViewController.addCell(sender:)))
-            self.navigationItem.setLeftBarButton(addButton, animated: true)
-        } else {
-            print("通常モード")
-            self.navigationItem.setLeftBarButton(nil, animated: true)
-        }
-    }
-    
 
     //addButtonが押された際呼び出される
    @IBAction func addCell(sender: AnyObject) {
         print("追加")
+    
+    //textの表示はalertのみ。ActionSheetだとtextfiledを表示させようとすると
+    //落ちます。
+    let alert:UIAlertController = UIAlertController(title:"Entry",
+                                                    message: "What happened today?",
+                                                    preferredStyle: UIAlertControllerStyle.alert)
+    
+    let cancelAction:UIAlertAction = UIAlertAction(title: "Cancel",
+                                                   style: UIAlertActionStyle.cancel,
+                                                   handler:{
+                                                    (action:UIAlertAction!) -> Void in
+                                                    print("Cancel")
+    })
+    let defaultAction:UIAlertAction = UIAlertAction(title: "Save",
+                                                    style: UIAlertActionStyle.default,
+                                                    handler:{
+                                                        (action:UIAlertAction!) -> Void in
+                                                        print("Save")
+    })
+    alert.addAction(cancelAction)
+    alert.addAction(defaultAction)
+
+    alert.addTextField(configurationHandler: {(text:UITextField!) -> Void in
+        //対象UITextFieldが引数として取得できる
+        text.placeholder = "Date"
+    })
+    alert.addTextField(configurationHandler: {(text:UITextField!) -> Void in
+        text.placeholder = "What happened?"
         
-        // myItemsに追加.
-        imgArray.append("img0.jpg")
+    })
+
+//    //textfieldの追加
+//    alert.addTextField(configurationHandler: {(text:UITextField!) -> Void in
+//    })
+//    //実行した分textfieldを追加される。
+//    alert.addTextField(configurationHandler: {(text:UITextField!) -> Void in
+//    })
+    present(alert, animated: true, completion: nil)
+    
+//    alert.addTextField(configurationHandler: {(text:UITextField!) -> Void in
+//        //対象UITextFieldが引数として取得できる
+//        text.placeholder = "Date"
+//    })
+//    alert.addTextField(configurationHandler: {(text:UITextField!) -> Void in
+//        text.placeholder = "What happened?"
+//        
+//    })
+    
+//        // myItemsに追加.
+//        imgArray.append("img0.jpg")
+//        entryArray.append("2017/03/19")
     
         // TableViewを再読み込み.
         dateTableView.reloadData()
@@ -68,16 +98,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //Cellを挿入または削除しようとした際に呼び出される
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         
-        // 削除のとき.
-        if editingStyle == UITableViewCellEditingStyle.delete {
-            print("削除")
-            
-            // 指定されたセルのオブジェクトをmyItemsから削除する.
+        // 削除のとき
+        if editingStyle == .delete {
             imgArray.remove(at: indexPath.row)
-            label2Array.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
             
-            // TableViewを再読み込み.
+            imgArray.remove(at: indexPath.row)
+            entryArray.remove(at: indexPath.row)
+            
             dateTableView.reloadData()
+            
         }
     }
 
@@ -85,7 +115,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
     /// セルの個数を指定するデリゲートメソッド（必須）
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -114,8 +143,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         label1.text = "No.\(indexPath.row + 1)"
         
         // Tag番号 ３ で UILabel インスタンスの生成
-        let label2 = dateTableView.viewWithTag(3) as! UILabel
-        label2.text = "\(label2Array[indexPath.row])"
+        let entry = dateTableView.viewWithTag(3) as! UILabel
+        entry.text = "\(entryArray[indexPath.row])"
         
         return cell
     }
