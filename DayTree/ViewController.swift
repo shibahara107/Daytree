d8f47d4f059032c51b3f2d70be0b92e1d4fe5177
Yoshi <yoshinori.shibahara@gmail.com>
2017-03-20 16:01:49 +0900

    Spring Camp 2017 Day 2

--- DayTree/ViewController.swift
+++ DayTree/ViewController.swift
@@ -18,10 +18,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet var editButton: UIButton!
     
     // section毎の画像配列
-    var imgArray: [String] = ["img0.jpg","img1.jpg","img2.jpg","img3.jpg", "img4.jpg","img5.jpg","img6.jpg","img7.jpg"]
+    var imgArray: [String] = ["img0.jpg"]
     
-    var entryArray: [String] = ["2017/02/10","2017/02/9","2017/02/8","2017/02/7",
-                                "2017/02/6","2017/02/5","2017/02/4","2017/02/3"]
+    var entryArray = [[Any]]()
     
     var dateArray: [String] = ["↑First Day↑"]
 
@@ -32,9 +31,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         dateTableView.delegate = self
         dateTableView.dataSource = self
         
+        entryArray.append(["0320","SpringCamp"])
     }
-    
-    //aaa
+
     
 
     //addButtonが押された際呼び出される
@@ -58,7 +57,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                                                     handler:{
                                                         (action:UIAlertAction!) -> Void in
                                                         print("Save")
+                                                        
+                                                        let dataText = alert.textFields![0].text! as String
+                                                        let contentText = alert.textFields![1].text! as String
+                                                            
+                                                        self.entryArray.append([dataText,contentText])
+                                                        
+//                                                        let textFields:Array<UITextField>? =  alert.textFields?[0] as Array<UITextField>?
+//                                                        if textFields != nil {
+//                                                            for textField:UITextField in textField.Array {
+//                                                                
+//                                                                self.entryArray.append(textField.text!)
+//                                                                self.imgArray.append("img0.jpg")
+//                                                                
+                                                                // TableViewを再読み込み.
+                                                                self.dateTableView.reloadData()
+//
+//                                                                //各textにアクセス
+//                                                                print(textField.text)
+//                                                            }
+//                                                        }
+
     })
+                                                        
     alert.addAction(cancelAction)
     alert.addAction(defaultAction)
 
@@ -102,13 +123,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         // 削除のとき
         if editingStyle == .delete {
-            imgArray.remove(at: indexPath.row)
+            entryArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
             
-            imgArray.remove(at: indexPath.row)
-            entryArray.remove(at: indexPath.row)
             
             dateTableView.reloadData()
+            
+            print("Delete")
             
         }
     }
@@ -120,7 +141,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     /// セルの個数を指定するデリゲートメソッド（必須）
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imgArray.count
+        return entryArray.count
     }
     
     /// セルに値を設定するデータソースメソッド（必須）
@@ -133,27 +154,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
        // cell.textLabel!.text = fruits[indexPath.row]
         
     
-        
-        let img = UIImage(named:"\(imgArray[indexPath.row])")
-        
+
+//            img = UIImage(named:"\(imgArray[indexPath.row])")
+        let img = UIImage(named:"img1.jpg")
         // Tag番号 1 で UIImageView インスタンスの生成
         let imageView = dateTableView.viewWithTag(1) as! UIImageView
         imageView.image = img
         
         // Tag番号 ２ で UILabel インスタンスの生成
         let label1 = dateTableView.viewWithTag(2) as! UILabel
-        label1.text = "No.\(indexPath.row + 1)"
+        label1.text = "\(entryArray[indexPath.row][0])"
         
         // Tag番号 ３ で UILabel インスタンスの生成
         let entry = dateTableView.viewWithTag(3) as! UILabel
-        entry.text = "\(entryArray[indexPath.row])"
+        entry.text = "\(entryArray[indexPath.row][1])"
         
         return cell
     }
     
     /// セルが選択された時に呼ばれるデリゲートメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("セル番号：\(indexPath.row) セルの内容：\(imgArray[indexPath.row]) ")
+        print("セル番号：\(entryArray[indexPath.row][0]) セルの内容：\(entryArray[indexPath.row][1]) ")
     }
 
 
